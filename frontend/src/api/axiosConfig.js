099b7fbe62b07dc731b25473589858a7b331b743
Madieyeee <junioranne1702@gmail.com>
2025-08-06 22:28:20 +0000

    Fix2

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000/api',
+  baseURL: process.env.REACT_APP_API_URL || 'https://snipzone.onrender.com/api',
 });
 
 export default axiosInstance;
