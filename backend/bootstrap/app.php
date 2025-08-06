95eddbb792dd185667201fe026bd5ea4810b7f74
Madieyeee <junioranne1702@gmail.com>
2025-08-06 23:17:40 +0000

    qwerty

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -11,7 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append("\Illuminate\Http\Middleware\HandleCors::class");
+        $middleware->validateCsrfTokens(except: [
+            'api/*'
+        ]);
+
+        $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
     })->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
